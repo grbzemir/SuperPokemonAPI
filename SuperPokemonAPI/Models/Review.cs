59d8ef49e73ead9e52a875f76100d4812be8d87b
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-19 14:43:47 +0300

    Entities

---
+++ SuperPokemonAPI/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperPokemonAPI.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+    }
+}
