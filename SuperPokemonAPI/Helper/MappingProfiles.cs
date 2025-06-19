427ac03fdfcb2f349d2dc9b5c3fd3fb6c7113e6c
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-20 02:32:31 +0300

    AutoMapper

--- SuperPokemonAPI/Helper/MappingProfiles.cs
+++ SuperPokemonAPI/Helper/MappingProfiles.cs
@@ -9,6 +9,7 @@ namespace SuperPokemonAPI.Helper
         public MappingProfiles()
         {
             CreateMap<Pokemon, PokemonDto>();
+            CreateMap<PokemonDto, Pokemon>();
             CreateMap<Category, CategoryDto>();
             CreateMap<CategoryDto, Category>();
             CreateMap<Country, CountryDto>();
