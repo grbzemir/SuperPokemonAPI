a8bc6c6e3047c33f1f0334fb2b5f4cceaad0f96e
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-19 18:04:31 +0300

    AddScopped

--- SuperPokemonAPI/Program.cs
+++ SuperPokemonAPI/Program.cs
@@ -12,7 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
