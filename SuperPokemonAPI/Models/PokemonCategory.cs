30f97baf00a21269957edb405a590aa68d2135db
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-19 14:55:29 +0300

    Entities

---
+++ SuperPokemonAPI/Models/PokemonCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperPokemonAPI.Models
+{
+    public class PokemonCategory
+    {
+        public int PokemonId { get; set; }
+        public int CategoryId { get; set; }
+        public Pokemon Pokemon { get; set; } // 1-1 relationship with Pokemon
+    }
+}
