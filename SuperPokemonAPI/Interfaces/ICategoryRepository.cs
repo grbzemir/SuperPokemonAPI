03763cd27a7c9e50d62eeb7acf12e7fcc8e1f101
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-19 18:03:41 +0300

    Interfaces

---
+++ SuperPokemonAPI/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using SuperPokemonAPI.Models;
+
+namespace SuperPokemonAPI.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonsByCategory(int categoryId);
+        bool CategoryExists(int id);
+    }
+}
