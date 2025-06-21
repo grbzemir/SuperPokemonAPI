bf434ddd608da31a0b74b2ef5f146342f031ce2b
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-21 15:32:52 +0300

    CountryRepository

--- SuperPokemonAPI/Repository/CountryRepository.cs
+++ SuperPokemonAPI/Repository/CountryRepository.cs
@@ -59,5 +59,11 @@ namespace SuperPokemonAPI.Repository
             var saved = _context.SaveChanges();
             return saved > 0 ? true : false;
         }
+
+        public bool UpdateCountry(Country country)
+        {
+            _context.Update(country);
+            return Save();
+        }
     }
 }
